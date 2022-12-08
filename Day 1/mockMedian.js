f4eba3f6c110a5751575fbf9cd9c9862afd8228f
satpute-soujanya <satputesoujanya@gmail.com>
2022-12-08 08:24:28 +0530

    median of even array

--- Day 1/mockMedian.js
+++ Day 1/mockMedian.js
@@ -1,17 +1,20 @@
-const arr = [1, 2, 3, 343, 34, 5, 7]
+const arr = [1, 2, 3, 343, 45, 34, 5, 7]
 
 function findMedian(arr) {
   let median = 0
   let sorted_array = arr.sort(function (a, b) {
     return a - b
   })
+  let ArrayMid = arr.length / 2
   console.log(sorted_array)
   if (arr.length % 2 !== 0) {
-    let medianIndex = Math.floor(arr.length / 2)
-    console.log(medianIndex)
+    let medianIndex = Math.floor(ArrayMid)
     median = sorted_array[medianIndex]
     console.log(median)
+  } else {
+    median = Math.floor((arr[ArrayMid] + arr[ArrayMid - 1]) / 2)
+    return median
   }
   // Write your code here
 }
-findMedian(arr)
+console.log(findMedian(arr))
